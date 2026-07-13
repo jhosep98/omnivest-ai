39bcc652b287d17f08600d8ef019e07ae4e984a6
jhosep98 <jhosep149@hotmail.com>
2026-07-13 18:56:36 -0300

    feat(frontend): scaffold Next.js app

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-3xl font-bold">Frontend</h1>
+    </div>
+  )
+}
